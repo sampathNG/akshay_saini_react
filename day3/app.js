16d0ba25efab2ea06e598a24bdac01199e96b365
sampathNG <jnanisampath@gmail.com>
2024-12-16 12:04:29 +0530

    day3 done

--- day3/app.js
+++ day3/app.js
@@ -5,5 +5,27 @@ const heading = React.createElement(
   { id: "heading" },
   "Hello World from React"
 );
+// const JsxHeading = <h1>Hello World from JSX</h1>;
+const JsxHeading = () => <h1>Hello World from JSX</h1>;
+// FUNCTIONAL COMPONENTENT
+const truth = true;
+
+const title = (
+  <div>
+    <h1>sampath title</h1>
+  </div>
+);
+const HeadingComponent = () => {
+  return (
+    <>
+      {title}
+      <h2>{truth + 1}</h2>
+      <JsxHeading />
+      <h1>Hello World from Functional Component</h1>
+    </>
+  );
+};
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
